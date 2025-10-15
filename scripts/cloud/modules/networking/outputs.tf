2ddf0cb27c1446b22523690336a5cac24414ab2d
Evangelos Pappas <epappas@evalonlabs.com>
2025-10-15 12:22:16 +0200

    feat(cloud): expose NAT Gateway public IP for validator whitelisting
    * Add nat_gateway_public_ip output to networking module
    * Expose NAT Gateway Elastic IP address in root outputs
    * Enable whitelisting of ECS service traffic on external validator servers
    * Provide static IP for all outbound ECS traffic through single NAT Gateway

--- scripts/cloud/modules/networking/outputs.tf
+++ scripts/cloud/modules/networking/outputs.tf
@@ -44,6 +44,11 @@ output "nat_gateway_id" {
   value       = aws_nat_gateway.main.id
 }
 
+output "nat_gateway_public_ip" {
+  description = "NAT Gateway public IP address (use this to whitelist ECS services on external systems)"
+  value       = aws_eip.nat.public_ip
+}
+
 output "internet_gateway_id" {
   description = "Internet Gateway ID"
   value       = aws_internet_gateway.main.id
